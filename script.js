d6dcb62bcbde07a6a0d223629bacb83d9dc94304
Hectorg Higareda <hector.g.higareda@gmail.com>
2025-02-13 16:36:07 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-element.addEventListener ( 'pointerup' , ( event ) => { if (event. pointerType === "mouse" ) {} if (event. pointerType === "touch" ) {} if (event. pointerType === "pen" ) {} });
+element.addEventListener ( 'pointerup' , ( event ) => { 
+    if (event. pointerType === "mouse" ) {
+     if (event. pointerType === "touch" ) {
+     if (event. pointerType === "pen" ) }}});
+
 document.addEventListener('DOMContentLoaded', () => {
     const card = document.querySelector('.card');
     const canvas = document.getElementById('scratchCanvas');
